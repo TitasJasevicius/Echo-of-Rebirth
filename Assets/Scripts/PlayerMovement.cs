bcf0ddaa6cd20ee0cb970ce66eb8ccce3c2ab3db
TitasJasevicius <titasjasevicius@gmail.com>
2025-03-21 15:57:15 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jumpPressed = false;
+    bool jumpHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+            jumpHeld = true;
+        }
+
+        if (Input.GetButton("Jump")) // while holding space
+        {
+            jumpHeld = true;
+        }
+
+        if (Input.GetButtonUp("Jump")) // if no longer holding space
+        {
+            jumpHeld = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jumpPressed, jumpHeld);
+        jumpPressed = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Echo-of-Rebirth
+ 
